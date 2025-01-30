7cc4d8f11a1b68ef84540144273efb7374e92bd6
Maycon Deivison <m.deivison.d@gmail.com>
2025-01-30 20:41:58 -0300

    curso

---
+++ app.js
@@ -0,0 +1,40 @@
+let listaAmigos = [];
+
+function adicionarAmigo() {
+    let nomeDoAmigo = document.querySelector('input').value;
+    if (nomeDoAmigo == '') {
+        alert('Por favor, digite um nome.');
+        return false;
+    } else {
+        limparLista();
+        listaAmigos.push(nomeDoAmigo); 
+        let lista = document.getElementById('listaAmigos');
+        for (i = 0; i < listaAmigos.length; ++i) {
+            let li = document.createElement('li');
+            li.innerText = listaAmigos[i];
+            lista.appendChild(li);
+        }
+        limparCampo();
+    }
+}
+
+function sortearAmigo() {
+    if (listaAmigos == '') {
+        alert('Por favor, insira um nome.');
+    } else {
+        let amigoSecreto = listaAmigos[Math.floor(Math.random() * listaAmigos.length)];
+        let amigo = document.querySelector('resultado');
+        resultado.innerHTML = amigoSecreto;
+    }
+    limparLista();
+}
+
+// limpar campo do input após adc nome do amigo
+function limparCampo() {
+    nomeDoAmigo = document.querySelector('input');
+    nomeDoAmigo.value = '';
+}
+function limparLista() {
+    lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+}
